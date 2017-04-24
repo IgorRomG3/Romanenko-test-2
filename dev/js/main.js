a943d38f3c7656f637558c15032a61c1974c72d7
IgorRomG3 <webroma57@gmail.com>
2017-04-24 18:04:55 +0300

    Some fixes for main.js and styles

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,19 +1,30 @@
 var accordeon = document.getElementsByClassName('accordeon__menu')[0],
     accordeonBtnPlus = document.getElementsByClassName('accordeon__plus'),
-    accordeonBtnMinus = document.getElementsByClassName('accordeon__minus');
+    accordeonBtnMinus = document.getElementsByClassName('accordeon__minus'),
+    accordeonTitle = document.getElementsByClassName('accordeon__title');
 
 accordeon.onclick = function(e){
     e = e || event;
     var target = e.target;
     for(i = 0; i < accordeonBtnPlus.length ; i++ ){
+
         if(target == accordeonBtnPlus[i]){
-            accordeonBtnPlus[i].style.display = 'none';
-            accordeonBtnMinus[i].style.display = 'block';
+            accordeonBtnPlus[i].classList.toggle('accordeon__plus_hidden');
+
+            accordeonBtnMinus[i].classList.toggle('accordeon__minus_visiable');
+
+            accordeonTitle[i].classList.toggle('accordeon__title_dark');
+
             accordeonBtnPlus[i].parentNode.parentNode.classList.toggle('accordeon__item_opened');
         }
+        
         if(target == accordeonBtnMinus[i]){
-            accordeonBtnPlus[i].style.display = 'block';
-            accordeonBtnMinus[i].style.display = 'none';
+            accordeonBtnPlus[i].classList.toggle('accordeon__plus_hidden');
+
+            accordeonBtnMinus[i].classList.toggle('accordeon__minus_visiable');
+
+            accordeonTitle[i].classList.toggle('accordeon__title_dark');
+
             accordeonBtnMinus[i].parentNode.parentNode.classList.toggle('accordeon__item_opened');
         }
     }
