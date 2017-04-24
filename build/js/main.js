14f141bff06f16d54c496d30802bff4097db039f
IgorRomG3 <webroma57@gmail.com>
2017-04-24 03:06:14 +0300

    corrected html, file structure, css classes, js-file

--- build/js/main.js
+++ build/js/main.js
@@ -1,6 +1,6 @@
-var accordeon = document.getElementsByClassName('accordeon')[0],
-    accordeonBtnPlus = document.getElementsByClassName('accordeon__js_plus'),
-    accordeonBtnMinus = document.getElementsByClassName('accordeon__js_minus');
+var accordeon = document.getElementsByClassName('accordeon__menu')[0],
+    accordeonBtnPlus = document.getElementsByClassName('accordeon__plus'),
+    accordeonBtnMinus = document.getElementsByClassName('accordeon__minus');
 
 accordeon.onclick = function(e){
     e = e || event;
@@ -9,12 +9,12 @@ accordeon.onclick = function(e){
         if(target == accordeonBtnPlus[i]){
             accordeonBtnPlus[i].style.display = 'none';
             accordeonBtnMinus[i].style.display = 'block';
-            accordeonBtnPlus[i].parentNode.classList.toggle('accordeon__item_opened');
+            accordeonBtnPlus[i].parentNode.parentNode.classList.toggle('accordeon__item_opened');
         }
         if(target == accordeonBtnMinus[i]){
             accordeonBtnPlus[i].style.display = 'block';
             accordeonBtnMinus[i].style.display = 'none';
-            accordeonBtnMinus[i].parentNode.classList.toggle('accordeon__item_opened');
+            accordeonBtnMinus[i].parentNode.parentNode.classList.toggle('accordeon__item_opened');
         }
     }
 }
